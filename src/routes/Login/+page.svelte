ece5a0b78dbf04c44840d9e8d03eb91643fbd0fd
Your Name <ryanrob327@gmail.com>
2023-02-04 21:52:20 -0800

    cleaned create account page and added login page

---
+++ src/routes/Login/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Don't Leave Me</title>
+	<meta name="description" content="Don't Leave Me (Dontavious): Exclusive dating app for high schoolers by high schoolers" />
+</svelte:head>
+
+
+
+<div class="text-white uppercase mx-auto pt-16 pb-[3%] relative z-0 flex flex-col items-center justify-center">
+	<h1 class="tracking-[1.5rem] text-5xl text-center pb-[20%]">
+        LOGIN
+	</h1>
+    <p class="text-1xl text-center">
+        DONT HAVE AN ACCOUNT? <a href="http://localhost:5173/CreateAccount" class="text-button"> CREATE ONE</a>
+    </p>
+</div>
+
+
+<div class="text-white uppercase mx-auto pt-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
+	<input type="text" 
+		id="username" 
+		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
+		placeholder="USERNAME" style="width: 500px; text-align: center;" autocomplete="off" required>
+</div>
+
+<div class="text-white uppercase mx-auto pt-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
+	<input type="password" 
+		id="password" 
+		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
+		placeholder="PASSWORD" style="width: 500px; text-align: center;" autocomplete="off" required>
+</div>
+
+
+<div class="text-white uppercase mx-auto pt-[5%] pb-[5%] px-80 relative z-0 flex flex-col items-center justify-center">
+	<a href="http://localhost:5173/">
+		<button class="bg-hot-pink hover:bg-hot-pink-hover text-white py-2 px-4 rounded-full align-center px-20">
+			<h1 class=" text-3xl text-center">LOGIN</h1>
+		</button>
+	</a>	
+</div>
