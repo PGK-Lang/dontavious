0f1f28efa3dc945c8867443913b65cb5d40acede
Your Name <ryanrob327@gmail.com>
2023-02-07 19:09:44 -0800

    implementing input box component

--- src/routes/Login/+page.svelte
+++ src/routes/Login/+page.svelte
@@ -1,8 +1,9 @@
 <script>
-import { authHandlers } from "../../lib/firebase/firebase.client"
+	import InputBox from "$lib/components/InputBox.svelte";
+	import { authHandlers } from "../../lib/firebase/firebase.client"
 
-let email = ''
-let password = ''
+	let email = ''
+	let password = ''
 </script>
 
 <svelte:head>
@@ -10,30 +11,30 @@ let password = ''
 	<meta name="description" content="Don't Leave Me (Dontavious): Exclusive dating app for high schoolers by high schoolers" />
 </svelte:head>
 
+
+<!-- create account page title -->
 <div class="text-white uppercase mx-auto pt-16 pb-[3%] relative z-0 flex flex-col items-center justify-center">
 	<h1 class="tracking-[1.5rem] text-5xl text-center pb-[20%]">
         login
 	</h1>
+	<!-- link to sign up -->
     <p class="text-1xl text-center uppercase">
-        don't have an account? <a href="/login/signup" class="text-button">sign up!</a>
+        don't have an account? <a href="/Login/signup" class="text-button">sign up!</a>
     </p>
 </div>
 
 
-<div class="text-white uppercase mx-auto pt-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
-	<input bind:value={email} type="text" 
-		id="username" 
-		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
-		placeholder="USERNAME" style="width: 500px; text-align: center;" autocomplete="off" required>
-</div>
+<form class="bg-black rounded px-8 pt-6 pb-8 mb-4">
+<!-- Email -->
+<InputBox type="text" id="email" placeholder="EMAIL" />
 
-<div class="text-white uppercase mx-auto pt-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
-	<input bind:value={password} type="password" 
-		id="password" 
-		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
-		placeholder="PASSWORD" style="width: 500px; text-align: center;" autocomplete="off" required>
-</div>
 
+<!-- Password -->
+<InputBox type="password" id="password" placeholder="PASSWORD" />
+</form>
+
+
+<!-- Login Button -->
 <div class="text-white uppercase mx-auto pt-[5%] pb-[5%] px-80 relative z-0 flex flex-col items-center justify-center">
 	<a href="/">
 		<button class="bg-hot-pink hover:bg-hot-pink-hover text-white py-2 rounded-full align-center px-20">
--- src/routes/Login/signup/+page.svelte
+++ src/routes/Login/signup/+page.svelte
@@ -1,9 +1,7 @@
 <script>
 // @ts-nocheck
-
+	import InputBox from "$lib/components/InputBox.svelte";
     import { authHandlers } from "$lib/firebase/firebase.client";
-
-
 </script>
 
 <svelte:head>
@@ -11,49 +9,41 @@
 	<meta name="description" content="Don't Leave Me (Dontavious): Exclusive dating app for high schoolers by high schoolers" />
 </svelte:head>
 
+
+<!-- create account page title -->
 <div class="text-white uppercase mx-auto pt-16 pb-[2%] relative z-0 flex flex-col items-center justify-center">
 	<h1 class="tracking-[1.5rem] text-5xl text-center">
         CREATE ACCOUNT
 	</h1>
 </div>
+
+
 <form class="bg-black rounded px-8 pt-6 pb-8 mb-4">
-<div class="text-white uppercase mx-auto pt-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
-	<input type="text" 
-		id="first_name" 
-		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
-		placeholder="FIRST NAME" style="width: 500px; text-align: center;" autocomplete="off" required>
-</div>
-
-<div class="text-white uppercase mx-auto pt-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
-	<input type="text" 
-		id="last_name" 
-		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
-		placeholder="LAST NAME" style="width: 500px; text-align: center;" autocomplete="off" required>
-</div>
-
-<div class="text-white uppercase mx-auto pt-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
-	<input
-	type="text" 
-		id="email" 
-		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
-		placeholder="USERNAME" style="width: 500px; text-align: center;" autocomplete="off" required>
-</div>
-
-<div class="text-white uppercase mx-auto pt-[4%] px-80 relative z-0 flex flex-col items-center justify-center">
-	<input type="password" 
-		id="password" 
-		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
-		placeholder="PASSWORD" style="width: 500px; text-align: center;" autocomplete="off" required>
-</div>
-
-<div class="text-white uppercase mx-auto pt-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
-	<input type="password" 
-		id="comfirm_password" 
-		class="bg-hot-pink border border-hot-pink text-white text-sm rounded-lg focus:bg-hot-pink focus:border-hot-pink block w-full p-2.5 dark:bg-hot-pink dark:border-hot-pink dark:placeholder-white dark:text-white dark:focus:ring-hot-pink dark:focus:border-hot-pink" 
-		placeholder="CONFIRM_PASSWORD" style="width: 500px; text-align: center;" autocomplete="off" required>
-</div>
+<!-- first name -->
+<InputBox type="text" id="first_name" placeholder="FIRST NAME" />
 
 
+<!-- Last name -->
+<InputBox type="text" id="last_name" placeholder="LAST NAME" />
+
+
+<!-- Email -->
+<InputBox type="text" id="email" placeholder="EMAIL" />
+
+
+<!-- Blank space -->
+<div class="pb-[4%]"></div>
+
+
+<!-- password -->
+<InputBox type="password" id="password" placeholder="PASSWORD" />
+
+
+<!-- confirm password -->
+<InputBox type="password" id="confirm_password" placeholder="CONFIRM PASSWORD" />
+
+
+<!-- 'next' button -->
 <div class="text-white uppercase mx-auto pt-[4%] pb-[1%] px-80 relative z-0 flex flex-col items-center justify-center">
 	<a href="http://localhost:5173/">
 		<button type="submit" on:click={authHandlers.signup("banana@gmail.com", "helloworld", "helloworlld")} class="bg-hot-pink hover:bg-hot-pink-hover text-white py-2 rounded-full align-center px-20">
