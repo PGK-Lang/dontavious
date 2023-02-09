d14dd38512d17d3842fc2c5003daa7a42e3d9176
rohin <rs.rohinsood@gmail.com>
2023-02-08 19:17:29 -0800

    revert adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
-import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter({
-			split: true
-		})
+		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
 
