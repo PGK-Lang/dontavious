0f6ebaeb00d96d6f55544cf6175bfff5dce8de5e
nVarap <nvarap@gmail.com>
2023-02-22 23:20:41 -0800

    Fraework for pseudo-input boxes. Please complete

---
+++ src/lib/components/ProfileInput.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let placeholder = 'placeholder'
+    export let state = true;
+	export let value = "";
+
+    
+
+</script>
+
+
+<input type="text"
+    disabled={state}
+	placeholder={placeholder} autocomplete="off" required
+	bind:value={value}>
+
+    <style> 
+        input {
+            /*Generic Style*/
+            background: pink;
+            color: black;
+            font-size: large;
+            text-transform: uppercase;
+            
+        }
+        /*only occurs if the input is disabled*/
+        input:disabled {
+            background: black;
+            
+        }
+    </style>
